14cf12744710d3a1a6dc042323d282c86bb4da41
Peter Ryder <pjr@brenclosures.com.au>
2020-02-03 13:24:51 +1000

    SPM tweaks

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "WSTagsField",
-            path: ".",
-            sources: ["Source"])
+        .target(name: "WSTagsField", dependencies: [])
     ]
 )
